c8574376265201f47494d443cb66d425a776f7fc
TEGDV <apjair97@gmail.com>
2021-06-14 10:41:42 -0500

    :art: Runs prettier

--- truffle-config.js
+++ truffle-config.js
@@ -3,19 +3,19 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "5777" // Match any network id
-    }
+      network_id: "5777", // Match any network id
+    },
   },
   compilers: {
     solc: {
       version: "^0.8.0", // A version or constraint - Ex. "^0.5.0"
-      parser: "solcjs",  // Leverages solc-js purely for speedy parsing
+      parser: "solcjs", // Leverages solc-js purely for speedy parsing
       settings: {
         optimizer: {
           enabled: true,
-          runs: 200   // Optimize for how many times you intend to run the code
-        }
-    }
-  }
-}
-}
+          runs: 200, // Optimize for how many times you intend to run the code
+        },
+      },
+    },
+  },
+};
